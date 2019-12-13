604c13a282448ea4b1f229d6eb52ed9f2d77d7eb
Igor Kuzevanov <igor.kuzevanov@profinit.eu>
2019-12-13 15:09:08 +0100

    Add query categorization

--- src/main/java/org/cz/cvut/fel/kuzevigo/sparqlingbackend/controller/QueryCategorizationController.java
+++ src/main/java/org/cz/cvut/fel/kuzevigo/sparqlingbackend/controller/QueryCategorizationController.java
@@ -1,12 +1,16 @@
 package org.cz.cvut.fel.kuzevigo.sparqlingbackend.controller;
 
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
+import org.cz.cvut.fel.kuzevigo.sparqlingbackend.dao.CategorizationRepository;
 import org.cz.cvut.fel.kuzevigo.sparqlingbackend.dao.CategoryInCategorizationRepository;
 import org.cz.cvut.fel.kuzevigo.sparqlingbackend.dao.QueryCategorizationRepository;
+import org.cz.cvut.fel.kuzevigo.sparqlingbackend.dao.QueryDocumentRepository;
 import org.cz.cvut.fel.kuzevigo.sparqlingbackend.dto.QueryCategorizationDto;
 import org.cz.cvut.fel.kuzevigo.sparqlingbackend.model.CategoryInCategorization;
 import org.cz.cvut.fel.kuzevigo.sparqlingbackend.model.QueryCategorization;
+import org.cz.cvut.fel.kuzevigo.sparqlingbackend.model.QueryDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -25,6 +29,12 @@ public class QueryCategorizationController {
     QueryCategorizationRepository queryCategorizationRepository;
 
     @Autowired
+    CategorizationRepository categorizationRepository;
+
+    @Autowired
+    QueryDocumentRepository queryDocumentRepository;
+
+    @Autowired
     CategoryInCategorizationRepository categoryInCategorizationRepository;
 
     @GetMapping("/queryCategorizations")
@@ -33,9 +43,12 @@ public class QueryCategorizationController {
     }
 
     @PostMapping("/createQueryCategorization")
-    public ResponseEntity createQueryCategorization(@RequestBody QueryCategorization dto) {
-        QueryCategorization queryCategorization = QueryCategorization.builder().queryDocument(dto.getQueryDocument()).categories(dto.getCategories()).build();
-        queryCategorizationRepository.save(queryCategorization);
+    public ResponseEntity createQueryCategorization(@RequestBody QueryCategorizationDto dto) {
+        QueryCategorization queryCategorization = QueryCategorization.builder().build();
+        queryCategorization.setCategorization(categorizationRepository.findById(dto.getCategorizationId()).orElseThrow(NoSuchElementException::new));
+        queryCategorization.setQueryDocument(queryDocumentRepository.save(dto.getQueryDocument()));
+        queryCategorization.setCategories(dto.getCategories());
+        queryCategorization = queryCategorizationRepository.save(queryCategorization);
         return ResponseEntity.ok().build();
     }
 
--- src/main/java/org/cz/cvut/fel/kuzevigo/sparqlingbackend/dto/QueryCategorizationDto.java
+++ src/main/java/org/cz/cvut/fel/kuzevigo/sparqlingbackend/dto/QueryCategorizationDto.java
@@ -1,17 +1,25 @@
 package org.cz.cvut.fel.kuzevigo.sparqlingbackend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.cz.cvut.fel.kuzevigo.sparqlingbackend.model.Categorization;
 import org.cz.cvut.fel.kuzevigo.sparqlingbackend.model.Category;
 import org.cz.cvut.fel.kuzevigo.sparqlingbackend.model.QueryDocument;
 
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class QueryCategorizationDto {
 
     Long id;
+    Long categorizationId;
     QueryDocument queryDocument;
-    List<Category> categories;
+    Set<Category> categories;
 }
